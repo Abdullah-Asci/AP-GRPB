251d769e16a3304edc44d6dcf12b95fb9084b77f
VincentChantraine <144010187+VincentChantraine@users.noreply.github.com>
2023-11-06 17:41:40 +0100

    Add Script

---
+++ Script_BD_AP2.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Ligue(
+   Id_L SMALLINT,
+   Nom_L VARCHAR(30),
+   PRIMARY KEY(Id_L)
+);
+
+CREATE TABLE Employer(
+   ID_E SMALLINT,
+   Nom_E VARCHAR(20),
+   Prenom VARCHAR(20),
+   Mail VARCHAR(50),
+   MDP VARCHAR(50),
+   Status VARCHAR(20),
+   Date_Dép DATE,
+   Date_Arri DATE,
+   Id_L SMALLINT NOT NULL,
+   PRIMARY KEY(ID_E),
+   FOREIGN KEY(Id_L) REFERENCES Ligue(Id_L)
+);
