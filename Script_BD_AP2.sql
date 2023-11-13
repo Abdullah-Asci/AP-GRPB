e90d4f00c3f5157a86ee441fcbd8ff6196d02222
VincentChantraine <144010187+VincentChantraine@users.noreply.github.com>
2023-11-13 16:09:30 +0100

    Update Script_BD_AP2.sql
    Upgrade + test

--- Script_BD_AP2.sql
+++ Script_BD_AP2.sql
@@ -1,3 +1,7 @@
+/*------------------------Tables-------------------------*/
+drop table if exists Ligue;
+drop table if exists Employer;
+
 CREATE TABLE Ligue(
    Id_L SMALLINT,
    Nom_L VARCHAR(30),
@@ -11,9 +15,23 @@ CREATE TABLE Employer(
    Mail VARCHAR(50),
    MDP VARCHAR(50),
    Status VARCHAR(20),
-   Date_Dép DATE,
-   Date_Arri DATE,
-   Id_L SMALLINT,
+   Date_Dep Date,
+   Date_Arri Date,
+   Id_L SMALLINT NOT NULL,
    PRIMARY KEY(ID_E),
    FOREIGN KEY(Id_L) REFERENCES Ligue(Id_L)
 );
+
+/*------------------------Test Enregistrement-------------------------*/
+
+/*Delete from Ligue;
+INSERT INTO Ligue (Id_L, Nom_L)
+VALUES
+   (1,"?");
+select * from Ligue;
+
+Delete from Employer;
+INSERT INTO Employer (ID_E, Nom_E, Prenom, Mail, MDP, Status, Date_Dep, Date_Arri, Id_L) 
+VALUES
+   (1,"?","?","?","?","?","2000-00-00","2000-00-00",1);
+select * from Employer;*/
